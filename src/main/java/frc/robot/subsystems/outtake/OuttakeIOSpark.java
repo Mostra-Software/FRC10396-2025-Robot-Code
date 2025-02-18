a6f787cb27b06fe7db5f5f6130e5bca63743428b
Tamer Guven <83493008+Tamy0@users.noreply.github.com>
2025-02-18 17:06:33 +0300

    Minor change from brushless to brushed outtake motor

--- src/main/java/frc/robot/subsystems/outtake/OuttakeIOSpark.java
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeIOSpark.java
@@ -21,18 +21,18 @@ import au.grapplerobotics.LaserCan;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import java.util.function.DoubleSupplier;
 
 public class OuttakeIOSpark implements OuttakeIO {
-  private SparkMax outtakeMotor = new SparkMax(outtakeCanId, MotorType.kBrushed);
+  private SparkFlex outtakeMotor = new SparkFlex(outtakeCanId, MotorType.kBrushed);
   private LaserCan lc = new LaserCan(SensorID);
 
   public OuttakeIOSpark() {
 
-    var master_config = new SparkMaxConfig();
+    var master_config = new SparkFlexConfig();
 
     master_config
         .idleMode(IdleMode.kCoast)
