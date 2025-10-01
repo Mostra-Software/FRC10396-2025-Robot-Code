ba5046358f857d6c351eee2ec31e8b10bd1a7ea0
Tamy0 <tamerguven07@hotmail.com>
2025-10-01 16:21:35 +0100

    Minor Fix

--- src/main/java/frc/robot/util/TargetingSystem.java
+++ src/main/java/frc/robot/util/TargetingSystem.java
@@ -33,7 +33,7 @@ public class TargetingSystem {
       new Transform2d((0.883 / 2.0) + 0.1, -Inches.of(1.0).in(Meters), Rotation2d.fromDegrees(183));
 
   private Transform2d rightrobotBranchScoringOffset =
-      new Transform2d((0.883 / 2.0) + 0.1, Inches.of(1.0).in(Meters), Rotation2d.fromDegrees(177));
+      new Transform2d((0.883 / 2.0) + 0.1, Inches.of(1.0).in(Meters), Rotation2d.fromDegrees(187));
   private Transform2d robotHPOffset =
       new Transform2d((0.883 / 2.0) + 0.1, Inches.of(0).in(Meters), Rotation2d.fromDegrees(0));
 
